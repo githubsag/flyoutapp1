e28e637301d157d740fcf9f1ffa42308ad8d4869
sohrab <azamighadim2@gmail.com>
2024-08-13 23:55:12 +0200

    added project flyoutapp1

---
+++ flyoutapp1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using flyoutapp1.Services;
+using flyoutapp1.Views;
+
+namespace flyoutapp1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
