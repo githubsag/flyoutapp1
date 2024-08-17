36e1e488f897243db23991729d77340e692ccd56
githubsag <sohrabag@kth.se>
2024-08-17 18:01:21 +0200

    Added RegisterRoute(name(LoginPage, typeof(LoginPage)) in file AppShell.xaml.cs

--- flyoutapp1/AppShell.xaml.cs
+++ flyoutapp1/AppShell.xaml.cs
@@ -13,6 +13,7 @@ namespace flyoutapp1
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
